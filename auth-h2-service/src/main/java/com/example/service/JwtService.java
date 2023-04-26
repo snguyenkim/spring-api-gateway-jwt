215c63714e0700a081f51358670d52ced3a041b3
sonnguyenkim <sonnguyenkim@gmail.com>
2023-04-26 17:33:02 -0400

    Implement API-Gateway Security with Auth-Service

--- auth-h2-service/src/main/java/com/example/service/JwtService.java
+++ auth-h2-service/src/main/java/com/example/service/JwtService.java
@@ -14,14 +14,11 @@ import java.util.Map;
 @Component
 public class JwtService {
 
-
     public static final String SECRET = "5367566B59703373367639792F423F4528482B4D6251655468576D5A71347437";
-
 
     public void validateToken(final String token) {
         Jwts.parserBuilder().setSigningKey(getSignKey()).build().parseClaimsJws(token);
     }
-
 
     public String generateToken(String userName) {
         Map<String, Object> claims = new HashMap<>();
